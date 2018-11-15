7f25f2b2420bc6608203c3b3541b74933a90d31a
Steven Blair <1outof8@gmail.com>
2018-11-14 18:32:30 -0600

    Initial Commit

---
+++ week2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  week2
+//
+//  Created by Steven Blair on 11/14/18.
+//  Copyright © 2018 Steven Blair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
